52355a85e0c984ac6e39e4508bfa7a5e26a61289
stropee <stropee@campus-eni.fr>
2024-06-05 16:59:27 +0200

    ajout commentaires

--- module_3/demo2/script.js
+++ module_3/demo2/script.js
@@ -1,8 +1,11 @@
 let pikachu;
-
+//utilisation de fetch pour récupérer des ressources (d'un api ou autre)
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu"/*,
     {method: "POST", body: JSON.stringify({})}*/)
+    //.then() permet d'éxécuter du code une fois que la reponse est revenue
     .then(response => response.json())
+    //l'utilisation de response.json() renvoie une Promise, le résultat étant passé automatique en paramètre
+    //du .then() qui suit
     .then(data => {
         pikachu = data
         let image = document.createElement('img')
@@ -10,4 +13,5 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu"/*,
         document.body.append(image)
     })
 
+//a ce moment là pikachu est vide car le script a continué son éxécution
 console.log(pikachu)
--- module_3/demo3/script.js
+++ module_3/demo3/script.js
@@ -1,3 +1,4 @@
+//fonction qui va mettre quelques secondes à s'éxécuter
 async function takeALotOfTime(index) {
     let i = 0
 
@@ -14,7 +15,8 @@ async function takeALotOfTime(index) {
 // const getAnimal = function(index){
 //
 // }
-
+//fonction fléchée stockée dans une variable js
+//le mot-clé async permet de transformer le retour d'une fonction en Promise
 const getAnimal = async (index) => {
     const aAnimals = {
         1: "Chat",
@@ -26,15 +28,22 @@ const getAnimal = async (index) => {
     takeALotOfTime(index)
     return aAnimals[index]
 }
+
+//ici on utilise await pour attendre la résolution de notre premier appel à getAnimal()
+//avant de passer au suivant, intéressant si on a besoin des données du premier pour faire le second appel
+//sinon c'est moins performant
 const zoo = async () => {
     const cat = await getAnimal(1)
     const giraffe = await getAnimal(2)
     return [cat, giraffe]
 }
 
+//ici on lance les 2 appels à getAnimal de manière simultané
+//on attend que les 2 appels soient terminés pour continuer -> plus performant
 const zoo2 = async () => {
     const cat =  getAnimal(3)
     const giraffe = getAnimal(4)
+    //Promise.all() permet d'attendre la résolution des 2 promesses avant de continuer
     return await Promise.all([cat, giraffe])
 }
 
@@ -44,9 +53,11 @@ zoo2().then(data => console.log(data))
 console.log("Fin du programme")
 
 
+//exemple avec de l'utilisation de fetch avec avec await et async
 async function bidule(){
     let response = await fetch('url')
 
+    //response.ok ou response.status == 200 permet de s'assurer du bon fonctionnement de notre appel
     if(response.ok){
         return response.json()
     }
@@ -54,7 +65,6 @@ async function bidule(){
 
  async function useBidule(){
     let data = await  bidule()
-    data.then()
 }
 
 
